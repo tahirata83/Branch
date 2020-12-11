e2d860c7b279227bc9a8589e5cef2054026d09b4
tahirata83 <tahiratayev@ichc.ro>
2020-12-12 01:39:01 +0200

    commit 3

--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
         System.out.println("Urunleri listele kismi yazildi");
         System.out.println("Sepete ekle kismi yazildi");
         System.out.println("commit 2 ye hazir");
+        System.out.println("alternatif kodlara baslayalim");
     }
 }
