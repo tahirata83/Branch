2b1d304796a95b5386fc9d4a13d63b21f94eabe1
tahirata83 <tahiratayev@ichc.ro>
2020-12-12 01:37:37 +0200

    commit 1

---
+++ src/Test1.java
@@ -0,0 +1,6 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Urunleri listele kismi yazildi");
+        System.out.println("Sepete ekle kismi yazildi");
+    }
+}
